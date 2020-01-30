3e44ae9aeadd0f2b3b9a3192dffaa9237c1d7ed6
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-01-30 13:18:23 +0330

    add bitcoin price  in rials

--- Bitcoin Price.py
+++ Bitcoin Price.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup
 
 people = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 people_json  = people.json()
-B = people_json["bpi"]["USD"]["rate"]
+B = people_json["bpi"]["USD"]["rate"].split(".")[0].replace("," , "")
 print("Bitcoin Price: "+ B + people_json["bpi"]["USD"]["description"])
 
 page = requests.get("https://www.tgju.org/chart/price_dollar_rl")
 soup = BeautifulSoup(page.content, 'html.parser')
-D = soup.find("span", itemprop="price").get_text()
+D = soup.find("span", itemprop="price").get_text().replace("," , "")
 print("Dolar Price :" + D + " Rials")
 bitcoinrial = int(B) * int(D)
 
