430afebfffac960774c45838dc8b7f2d14a8f4fe
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-02-05 17:31:09 +0330

    some changes in graph and add photo

--- Bitcoin Price.py
+++ Bitcoin Price.py
@@ -7,29 +7,28 @@ from bs4 import BeautifulSoup
 
 root= tk.Tk() 
 
-xlist2 = [0]
-ylist2 = [0]
+xlist2 = []
+ylist2 = []
 
 
 
-people = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-people_json  = people.json()
-B = int(people_json["bpi"]["USD"]["rate"].split(".")[0].replace("," , ""))
+myapi = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+myapi_json  = myapi.json()
+B = int(myapi_json["bpi"]["USD"]["rate"].split(".")[0].replace("," , ""))
 xlist2.append(B)
 
 
 page = requests.get("https://www.tgju.org/chart/price_dollar_rl")
 soup = BeautifulSoup(page.content, 'html.parser')
 D = int(soup.find("span", itemprop="price").get_text().replace("," , ""))
-bitcoinrial = B * D
-ylist2.append(bitcoinrial)
-    
+ylist2.append(D)
 
 
-data2 = {'Year':xlist2 ,
-         'Unemployment_Rate': ylist2
+
+data2 = {'Bitcoin \$':xlist2 ,
+         'Bitcoin_Dolar_Rial_Rate': ylist2
         }
-df2 = DataFrame(data2,columns=['Year','Unemployment_Rate'])
+df2 = DataFrame(data2,columns=['Bitcoin \$','Bitcoin_Dolar_Rial_Rate'])
 
 
 
@@ -46,18 +45,21 @@ df2 = DataFrame(data2,columns=['Year','Unemployment_Rate'])
 
 
 
-
-
-
-figure2 = plt.Figure(figsize=(15,5), dpi=200)
+figure2 = plt.Figure(figsize=(15,5), dpi=100)
 ax2 = figure2.add_subplot(111)
 line2 = FigureCanvasTkAgg(figure2, root)
 line2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-df2 = df2[['Year','Unemployment_Rate']].groupby('Year').sum()
+df2 = df2[['Bitcoin \$','Bitcoin_Dolar_Rial_Rate']].groupby('Bitcoin \$').sum()
 df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
-ax2.set_title('Year Vs. Unemployment Rate')
+ax2.set_title('Bitcoin price : ' +  str(xlist2[0]) + '\$ VS Dolar price : ' + str(ylist2[0])+ 'Rials')
+ax2.set_title('Dolar price : ' + str(ylist2[0])+ ' Rials    VS    Bitcoin price : ' +  str(xlist2[0]) + ' \$')
+
+
+
+
 
 
  
 
 root.mainloop()
+
